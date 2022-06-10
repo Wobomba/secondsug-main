c2bb723514ac6fafc1679a5e22ec9013b22b02b4
Wobomba <iwobomba999@gmail.com>
2022-06-10 16:29:41 +0300

    Latest

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,119 +1,72 @@
 import React from 'react';
 import Footer from '../../components/Footer'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 function About() {
     return (
-        <div class='sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300'>
-            <div class='max-w-md mx-auto'>
-                <div>
-                    <section>
-                        <img
-                            className="inset-0 object-cover w-full h-[500px]"
-                            src="./about_img.jpeg"
-                            alt=""
-                        />
-
-                        {/* Page name */}
-                        <div className='absolute top-0 mt-32 items-center justify-between text-white flex sm:flex'>
-                            <div className="items-center ml-20">
-                                <h1
-                                    data-aos="fade-up"
-
-                                    data-aos-duration="1000"
-
-                                    className="font-sans font-bold text-7xl">About</h1>
-                            </div>
+        <div class="relative">
+            <div class='flex-container1'>
+                <div class='flex-item-right1'>
+                    {/*About background */}
+                    <img className='inset-0 object-cover w-[1366px] h-[450px]' src='./city.jpg' alt='' />
+                    <div className='absolute top-0 mt-20 items-center justify-between text-white'>
+                        <div className='items-center ml-20'>
+                            <h1 data-aos='fade-up' data-aos-duration='1000' className='font-sans font-bold text-7xl'>About us</h1>
                         </div>
-                    </section>
-                </div>
-
-                {/* Content_info */}
-
-                <div className="bg-white h-[100vh] flex-1 flex items-center">
-                    <div
-                        data-aos="fade-left"
-
-                        data-aos-duration="1000"
-                        className="flex-[0.5px] ml-10">
-                        <img
-
-                            className="insert-0 w-full h-[550px] "
-                            src="./appui.jpeg"
-                            alt=""
-                        />
-                    </div>
-                    <div className="flex-[0.5px] text-black pl-10">
-                        <p
-                            data-aos="fade-up"
-
-                            data-aos-duration="1000"
-                            className="font-sans text-xl pt-4">
-                            Seconds is a technology provider that operates and designs a mobile platform matching drivers to passengers on the go.
-                        </p>
                     </div>
                 </div>
-
-                <div className="bg-white h-[50vh] flex-1 flex items-center">
-                    <div className="flex-[0.5px] text-black pl-10">
-                        <p
-                            data-aos="fade-up"
-
-                            data-aos-duration="1000"
-                            className="font-sans text-xl pt-4">
-                            Seconds is demonetizing mobility to build technology solutions that transform people in Africa, customer solutions that everyone aspires to use and that enable people and businesses to grow.{' '}
-                        </p>
-                    </div>
-                    <div
-                        data-aos="fade-left"
-
-                        data-aos-duration="1000"
-                        className="flex-[0.5px] ml-10">
-                        <img
-
-                            className="insert-0 w-full h-[550px] "
-                            src="./build_bridge.jpeg"
-                            alt=""
-                        />
-                    </div>
-                </div>
-
-                {/* About information */}
-                <div>
-                    <section className='section text-center'>
-                        <div className='container'>
-                            <div className="bg-white h-[100vh] flex-1 flex items-center">
-                                <div className='col-md 4 text-left mx-auto'>
-                                    <h2 className='font-bold  text-center text-4xl'>Vision</h2>
-                                    <p className='text-xl text-left'>
-                                        Mobility that is open and enabled by people.
-                                    </p>
-                                </div>
-                                <div className='col-md 4 text-center mx-auto'>
-                                    <h6 className='font-bold mx-auto text-4xl'>Mission</h6>
-                                    <p className='text-xl text-center'>
-                                        To connect, empower, and impact lives.
-                                    </p>
-                                </div>
-                                <div className='col-md 4  text-right mx-auto'>
-                                    <h6 className='font-bold text-center text-4xl'>Statement</h6>
-                                    <p className='text-xl text-right'>
-                                        A world where anyone anywhere can transform their lives
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-
-                <Footer />
-
             </div>
 
+            <div className='flex-container1'>
+                <div className='flex-item-left1'>
+                    <p className='about_text'>Seconds is a technology provider that operates and designs a mobile platform matching drivers to passengers on the go.</p>
+                    <p className='content_text'>Seconds is demonetizing mobility to build technology solutions that transform people in Africa, customer solutions that everyone aspires to use and that enables people and businesses to grow.</p>
+                    {/*Mission */}
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+                            <Typography>Read our mission</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                To connect, empower, and impact lives.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    {/*Vision */}
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+                            <Typography >Read our vision</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Mobility that is open and enabled by people.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    {/*Statement */}
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+                            <Typography >Read our statement</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                A world where anyone anywherecan transform their lives.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            </div>
+            <Footer/>
         </div>
-
-
-
-
     );
 }
 export default About;
--- src/pages/Ferry/index.js
+++ src/pages/Ferry/index.js
@@ -1,70 +1,66 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react'
 import Footer from '../../components/Footer'
+import { Link } from 'react-router-dom'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-
 function Ferry() {
-
     const [selectedDate, setselectedDate] = useState(null)
-
     return (
-        <div class='sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300'>
+        <div className='relative'>
+            
             <div>
-                <div class='flex-container'>
-                    {/*Request a car */}
-                    <div className='flex-item-left'>
-                        <h1 data-aos="fade-up"
-                            data-aos-duration="1000"
-                            class="font-sans font-semibold text-5xl">Get a ferry</h1>
-                    </div>
-
-                    {/*Address Form */}
-                    <div className='flex-item-right'>
-                        <label className="inline-block 
-                        mb-1 text-xl font-medium pb-1">Address</label>
-
-                        <input placeholder="From" required type="text"
-                            className="flex-grow w-full h-12 px-4 text-black 
-                        font-medium  mb-2 transition duration-200 bg-white 
-                        border border-gray-300 rounded shadow-sm 
-                        appearance-none focus:border-deep-purple-accent-400 
-                        focus:outline-none focus:shadow-outline" />
-
-                        <input placeholder="To" required type="text"
-                            className="flex-grow w-full h-12 px-4 text-black 
-                         font-medium  mb-2 transition duration-200 bg-white 
-                         border border-gray-300 rounded shadow-sm appearance-none 
-                         focus:border-deep-purple-accent-400 focus:outline-none
-                          focus:shadow-outline" />
-
-                    </div>
-                </div>
-
                 <div className='flex-container'>
-                      {/* Trip date */}
-                      <div className='flex-item-right' data-aos = 'fade-up' data-aos-duration = '1000'>
-                        <DatePicker selected={selectedDate}
-                            onChange={date => setselectedDate(date)}
-                            dateFormat='dd/MM/yyyy'
-                            minDate={new Date()}
-                            showYearDropdown
-                            scrollableMonthYearDropdown
-                            placeholderText='Date' 
-                            className='board'/>
-                    </div>
-
-                    {/*Trip Text */}
-
+                    {/* Request a car*/}
                     <div className='flex-item-left'>
-                        <h1 data-aos="fade-up"
-                            data-aos-duration="1000"
-                            class="font-sans font-semibold text-5xl">Calendar</h1>
-                    </div>                    
+                        <h1 data-aos="fade-up" data-aos-duration="1000" 
+                        class="font-sans font-semibold text-5xl">Get a <span className='secondtext'>Ferry</span></h1>
+                    </div>
+                    
+                    {/*Address form */}
+                    <div className='flex-item-right'>
+                        <form data-aos="fade-up" data-aos-duration="1000" className='form-design' >
 
+                            <div class="mb-1 sm:mb-2">
+                                <label className="inline-block mb-1 text-xl font-medium pb-1">Address</label>
+                                <input
+                                    placeholder="From"
+                                    required
+                                    type="text"
+                                    className="flex-grow w-full h-12 px-4 text-black font-medium  mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                />
+                                <input
+                                    placeholder="To"
+                                    required
+                                    type="text"
+                                    className="flex-grow w-full h-12 px-4 text-black font-medium  mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                />
+                            </div>
+                            <h1 className="pt-4 text-xl">Date</h1>
+                            <div className = 'cards'>
+                                {/*Trip date */}
+                                <DatePicker selected = {selectedDate}
+                                onChange = {date => setselectedDate (date)}
+                                dateFormat = 'dd/MM/yyyy'
+                                minDate={new Date()}
+                                showYearDropdown
+                                scrollableMonthYearDropdown
+                                placeholderText='Date'
+                                className='board'/>
+                            </div>
+                            
+                            <button type="button" class="text-white bg-blue-700 
+						hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+						font-medium  w-full rounded-lg text-sm px-5 py-2.5 text-center 
+						mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-8">Confirm
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
-    );
+    )
 }
+
 export default Ferry;
